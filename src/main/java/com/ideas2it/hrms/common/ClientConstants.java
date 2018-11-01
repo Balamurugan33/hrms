adff8e0ce8dd41b5e2f9ccaf05befe12a22c0c4b
bala129 <balamurugan@ideas2it.com>
2018-11-01 22:06:12 +0530

    client dao and session

---
+++ src/main/java/com/ideas2it/hrms/common/ClientConstants.java
@@ -0,0 +1,14 @@
+package com.ideas2it.hrms.common;
+
+public class ClientConstants {
+    // Error messages
+    public static String ERROR_CREATE_CLIENT 
+        = "The client could not be created->";
+    public static String ERROR_UPDATE_CLIENT 
+        = "The client could not be updated->";
+    public static String ERROR_DELETE_CLIENT 
+        = "The client could not be deleted->";
+    public static String ERROR_RETRIEVE_CLIENT 
+        = "The client could not be retrieved";
+    
+}
