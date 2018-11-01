fd179305ab6b8077aefeacdb2f1dc2a1b5bfb5d5
Ganesh-Venkat <ganeshvenkat@ideas2it.com>
2018-11-01 22:01:31 +0530

    Added comments to app exception,logger

--- src/main/java/com/ideas2it/hrms/exception/AppException.java
+++ src/main/java/com/ideas2it/hrms/exception/AppException.java
@@ -1,13 +1,21 @@
 package com.ideas2it.hrms.exception;
 
+/**
+ * <p>
+ * Application level exception handler
+ * Appends wrapped custom exceptions with relevant message and forwards it to
+ * the parent class: Exception 
+ * </p>
+ * 
+ * @author Ganesh Venkat S
+ */
+
 public class AppException extends Exception {
     public AppException(String customMessage) {
-        // Call the super class constructor
         super(customMessage);
     }
 
     public AppException(String customMessage, Throwable cause) {
-        // Call the super class constructor
         super(customMessage, cause);
     }
 }
