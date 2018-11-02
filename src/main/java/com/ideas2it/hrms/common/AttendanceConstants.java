e99def6671cd92548526c6d83d7d372bf00f2764
Ganesh-Venkat <ganeshvenkat@ideas2it.com>
2018-11-02 19:51:57 +0530

    Attendance: Added DAO & Service layers

---
+++ src/main/java/com/ideas2it/hrms/common/AttendanceConstants.java
@@ -0,0 +1,15 @@
+package com.ideas2it.hrms.common;
+
+public class AttendanceConstants {
+    // AttendanceDaoImpl: Error messages to display to the user
+    public static String ERROR_CREATE_ATTENDANCE
+        = "The attendance could not be created: ";
+    public static String ERROR_RETRIEVE_ATTENDANCE
+        = "The attendance could not be retrieved ";
+    public static String ERROR_RETRIEVE_ATTENDANCES
+        = "The list of attendances could not be retrieved ";
+    public static String ERROR_UPDATE_ATTENDANCE 
+        = "The attendance could not be updated: ";
+    public static String ERROR_DELETE_ATTENDANCE 
+        = "The attendance could not be deleted: ";    
+}
